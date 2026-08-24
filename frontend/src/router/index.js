600395d7197fd01a70feb4be4ec06e012f4e5e9a
HR Trainer Dev <dev@hrtrainer.local>
2026-08-24 22:55:26 +0800

    web部署: 路由改hash模式(静态托管兼容)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from "vue-router"
 
 const routes = [
   { path: '/login', name: 'login', component: () => import('../views/LoginView.vue') },
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
